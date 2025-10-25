6829399d99681679b21262286434fce8be8d1965
jihwankim128 <jihwan1535@nate.com>
2025-10-25 12:11:39 +0900

    refactor: 입력 처리 메서드 책임 분리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,14 +5,12 @@ import racingcar.adapter.in.InputView;
 import racingcar.adapter.in.OutputView;
 import racingcar.adapter.out.RandomForwardCondition;
 import racingcar.application.RacingCarUseCase;
-import racingcar.domain.ForwardCondition;
 
 public class Application {
     public static void main(String[] args) {
         InputView inputView = new InputView();
         OutputView outputView = new OutputView();
-        ForwardCondition condition = new RandomForwardCondition();
-        RacingCarUseCase useCase = new RacingCarUseCase(condition);
+        RacingCarUseCase useCase = new RacingCarUseCase(new RandomForwardCondition());
 
         ConsoleRacingController controller = new ConsoleRacingController(inputView, outputView, useCase);
         controller.run();
--- src/main/java/racingcar/adapter/in/InputView.java
+++ src/main/java/racingcar/adapter/in/InputView.java
@@ -2,19 +2,33 @@ package racingcar.adapter.in;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class InputView {
 
     public List<String> readInputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String[] carNames = readLine().split(",");
-        return List.of(carNames);
+        return readTokens();
     }
 
     public int readAttemptCount() {
         System.out.println("시도할 횟수는 몇 회인가요?");
-        String trialCount = readLine();
-        return Integer.parseInt(trialCount);
+        return readNumber();
+    }
+
+    private List<String> readTokens() {
+        String[] tokens = readLine().split(",");
+        return Arrays.stream(tokens)
+                .map(String::trim)
+                .toList();
+    }
+
+    private int readNumber() {
+        try {
+            return Integer.parseInt(readLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("정수 숫자만 입력해주세요.");
+        }
     }
 }
